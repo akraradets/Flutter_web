ff2317530b390bf5b9b512f2b18c07c13f4459c9
Akraradet Sinsamersuk <akraradets@gmail.com>
2024-06-07 04:16:13 +0000

    add init message

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
+########
+# This is the init file for .devcontainer
+# You should not use this or try to change this.
+# Otherwise, the .devcontainer may filed to run correctly.
+########
+
 #!/bin/bash
 
 git config --global --add safe.directory ~/flutter 
+git config --global --add safe.directory /workspaces/flutter_web
 # git config --global --add safe.directory /usr/bin/flutter 
 dart pub global activate rps 
 flutter pub get
